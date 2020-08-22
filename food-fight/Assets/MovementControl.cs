3c4362bb7d7fef56380f3e7717d54d4e41db5f43
SerBABA <dan.r0n3n@gmail.com>
2020-08-22 19:52:33 +1200

    Simple side to side movment
    -Added simple side to side movment

---
+++ food-fight/Assets/MovementControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{
+
+
+    [Header("Attributes")]
+    public float speed = 2.5f;
+    public float maxSpeed = 10f;
+    public Rigidbody2D rb;
+
+    private float horizontalDirection = 0;
+    private float currentSpeed = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalDirection = Input.GetAxisRaw("Horizontal");
+        float addition = Time.deltaTime * speed * horizontalDirection;
+        if (currentSpeed+addition < maxSpeed) {
+            transform.position += transform.right * addition;
+            currentSpeed += addition;
+        }
+        else
+        {
+            currentSpeed = maxSpeed;
+        }
+    }
+}
