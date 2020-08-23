e60b4966553e142234075e4f0890d8f25b1102f2
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-08-23 22:22:02 +1200

    Update 1.03
    Added random gen start position
    Added attempt at creating more rooms
    Added many errors

---
+++ Food- Fight/Assets/Rooms/levelGeneration.cs
@@ -0,0 +1,67 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class levelGeneration : MonoBehaviour
+{
+
+    public Transform[] startingPositions;
+    public GameObject[] rooms;
+
+    private int direction;
+    public float moveAmount;
+
+    private float timeBtwRoom;
+    public float startTimeBtwRoom = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int randStartPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[randStartPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+
+    }
+
+    private void Move()
+    {
+
+        if (direction == 1 || direction == 2)
+        { //Move Right
+            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+            transform.position = newPos;
+
+        }
+        else if (direction == 3 || direction == 4)
+        { //Move Right
+            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+            transform.position = newPos;
+        }
+        else if (direction == 5)
+        {
+            Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+            transform.position = newPos;
+        }
+
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        direction = Random.Range(1, 6);
+    }
+
+    private void update()
+    {
+        if (timeBtwRoom <= 0)
+        {
+            Move();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else {
+            timeBtwRoom -= Time.deltaTime;
+        }
+    
+    }
+
+}
