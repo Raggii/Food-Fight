c73013ad8c75760c5abf9e7f3d83a5f42c58990e
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-08-25 21:16:02 +1200

    Update 1.05
    Rooms dont overlap

--- Food- Fight/Assets/Rooms/levelGeneration.cs
+++ Food- Fight/Assets/Rooms/levelGeneration.cs
@@ -11,6 +11,7 @@ public class levelGeneration : MonoBehaviour
     public GameObject[] rooms;
 
     private int direction;
+    private int previousDirection = 0;
     public float moveAmount;
 
     private float timeBtwRoom;
@@ -23,32 +24,49 @@ public class levelGeneration : MonoBehaviour
         transform.position = startingPositions[randStartPos].position;
         Instantiate(rooms[0], transform.position, Quaternion.identity);
 
-        direction = 1;// Random.Range(1, 6);
 
     }
 
     private void Move()
     {
-
-        if (direction == 1 || direction == 2)
+        if (direction == 1 || direction == 2 )
         { //Move Right
-            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
-            transform.position = newPos;
-
+            if (previousDirection == 3 || previousDirection == 4)
+            { // ILLEGAL ACTION
+                direction = Random.Range(1, 6);
+            }
+            else
+            {
+                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPos;
+                previousDirection = direction;
+                direction = Random.Range(1, 6);
+            }
         }
         else if (direction == 3 || direction == 4)
-        { //Move Right
-            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
-            transform.position = newPos;
+        { //Move Left
+            if (previousDirection == 1 || previousDirection == 2)
+            { // ILLEGAL ACTION
+                direction = Random.Range(1, 6);
+            }
+            else
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPos;
+                previousDirection = direction;
+                direction = Random.Range(1, 6);
+            }
+   
         }
         else if (direction == 5)
-        {
+        { // Move Down
             Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
             transform.position = newPos;
+            previousDirection = direction;
+            direction = Random.Range(1, 6);
         }
 
         Instantiate(rooms[0], transform.position, Quaternion.identity);
-        direction = 1;// Random.Range(1, 6);
     }
 
     void FixedUpdate()
