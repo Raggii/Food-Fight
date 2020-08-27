a2d5d94f99c8f1078e340c2168130d8058ab3087
SerBABA <dan.r0n3n@gmail.com>
2020-08-28 00:18:04 +1200

    Movement Testing 2.123

--- Food- Fight/Assets/MovementController.cs
+++ Food- Fight/Assets/MovementController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    public float speed;
+    [Header("Constants")]
+    //public float mass;
+    //public float floorDrag;
+    public float maxSpeed = 8f;
+    public float acceleration = 30f;
+    public float decclaration = 40f;
+    public float speedThreshold = 0.3f;
+
+    [Header("Components")]
     public Rigidbody2D rb;
 
-    private Vector2 dir;
-    private Vector2 movment;
+    private Vector2 dir = new Vector2(0, 0);
+    //private Vector2 movment = new Vector2(0, 0);
+    private Vector2 velocity = new Vector2(0, 0);
 
 
     public void Update()
@@ -18,12 +27,76 @@ public class MovementController : MonoBehaviour
     }
 
 
+    public float getNewVelocity(float direction, float velocityVal, float deltaTime)
+    {
+        float newVelocity = velocityVal;
+        if (velocityVal != direction * maxSpeed)
+        {
+
+            if (direction == 0) {
+                newVelocity = velocityVal + decclaration * deltaTime * -Mathf.Sign(velocityVal);
+
+                if (Mathf.Abs(newVelocity) <= speedThreshold)
+                {
+                    newVelocity = 0f;
+                }
+
+            } else {
+                newVelocity = velocityVal + acceleration * deltaTime * Mathf.Sign(direction);
+
+                if (Mathf.Abs(newVelocity) > maxSpeed)
+                {
+                    newVelocity = Mathf.Sign(direction) * maxSpeed;
+                }
+
+            }
+        }
+
+     return newVelocity;
+    }
+    
+
     void FixedUpdate()
     {
+        Vector2 firstVelocity = rb.velocity;
+        velocity.x = getNewVelocity(dir.x, rb.velocity.x, Time.fixedDeltaTime);
+        velocity.y = getNewVelocity(dir.y, rb.velocity.y, Time.fixedDeltaTime);
+        //rb.velocity = velocity;
 
-        movment.x = Time.fixedDeltaTime * dir.x * speed;
-        movment.y = Time.fixedDeltaTime * dir.y * speed;
 
-        rb.MovePosition(rb.position + movment);
+        Vector2 forces = new Vector2((velocity.x - firstVelocity.x) / Time.fixedDeltaTime, (velocity.y - firstVelocity.y) / Time.fixedDeltaTime);
+        
+        rb.AddForce(forces);
+        Debug.Log("Velocity: " + rb.velocity + ", Forces: " + forces);
     }
+
+    /*
+    private float getNewVelocity(float direction, float velocityValue)
+    {
+        float desiredVel = direction * maxSpeed;
+        if (velocityValue != desiredVel)
+        {
+            if (Mathf.Abs(velocityValue - desiredVel) < deltaVelX)
+            {
+                velocityValue = desiredVel;
+            }
+            else
+            {
+                if (desiredVel != 0)
+                {
+                    velocityValue += deltaVelX * Mathf.Sign(direction);
+                }
+                else
+                {
+                    velocityValue -= deltaVelX * Mathf.Sign(velocityValue);
+                }
+            }
+        }
+        return velocityValue;
+    }
+    main :::
+         velocity.x = getNewVelocity(dir.x, velocity.x);
+        velocity.y = getNewVelocity(dir.y, velocity.y);
+        rb.velocity = velocity;
+    */
 }
