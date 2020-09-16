67e994f79eff7f0dd6d05b98f41a53dfeb5a7653
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-09-16 20:50:27 +1200

    Update 2.00
    Added the fixed level Generation which looks awesome
    And Deleted a bunch of usless stuff

---
+++ Food- Fight/Assets/Rooms/SpawnDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDown : MonoBehaviour
+{
+    public GameObject[] objects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (objects.Length <= 0)
+        {
+            return;
+        }
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, transform.rotation * Quaternion.Euler(0f, 0f, 90f));
+    }
+}
