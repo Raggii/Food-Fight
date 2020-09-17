66efc6d482f63d69d5aeb3976dabaa43cc012391
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-09-18 00:31:53 +1200

    Update 2.01
    Added Camera Rooms

---
+++ Food- Fight/Assets/MoveCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        if ((player.transform.position.x - transform.position.x) >= 5)
+        {
+
+            transform.Translate(transform.right * 10);
+
+        }
+        else if ((player.transform.position.x - transform.position.x) <= -5)
+        {
+
+            transform.Translate(transform.right * -10);
+
+        }
+        else if ((player.transform.position.y - transform.position.y) >= 3) {
+
+            transform.Translate(transform.up * 6);
+
+        }
+        else if ((player.transform.position.y - transform.position.y) <= -3)
+        {
+            transform.Translate(transform.up * -6);
+
+        }
+
+    }
+}
