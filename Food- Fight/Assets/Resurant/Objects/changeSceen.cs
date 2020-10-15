1ed09db0c0c122701d3e7c4b740a23fb9a425cd4
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-10-16 01:18:47 +1300

    Update 1.552
    Added fixed update 1.51
    Added Fade in and out transitions

--- Food- Fight/Assets/Resurant/Objects/changeSceen.cs
+++ Food- Fight/Assets/Resurant/Objects/changeSceen.cs
@@ -8,17 +8,36 @@ public class changeSceen : MonoBehaviour
 
     public int index;
     public string lvlName;
+    public Animator fader;
+
+    public float transTime;
 
     void OnTriggerEnter2D(Collider2D other)
     {
 
         if (other.CompareTag("Player"))
         {
-
-            SceneManager.LoadScene(index);
-            //SceneManager.LoadScene(lvlName);
+            loadNewLevel();
 
         }
+    
+    }
+
+    public void loadNewLevel() 
+    {
+        StartCoroutine(LoadLevel(index));
+        //SceneManager.LoadScene(lvlName);
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        // play animation
+        fader.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transTime);
+
+        SceneManager.LoadScene(levelIndex);
+        //wait for animation
     
     }
 }
