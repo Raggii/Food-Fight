ea6e69d7beec96dc5df86a51326136c097c48a3d
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-10-17 13:37:27 +1300

    Update 1.61
    Added Working title screen

---
+++ Food- Fight/Assets/startScreen/startGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+
+    public int firstSceneNo; 
+
+
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene(firstSceneNo);
+    }
+}
