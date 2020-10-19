2741e116eafc9db0cf62da4a2f80f0ae09d21043
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-10-19 22:16:01 +1300

    Update 1.7
    Added lots
    Added also more
    Added pause menu

---
+++ Food- Fight/Assets/pauseMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu");
+        // also set time back to normal
+        // takes back to actual menu
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+            if (gameIsPaused)
+            {
+
+                Resume();
+
+            }
+            else {
+
+                Pause();
+            
+            }
+        
+        }
+
+    }
+
+    
+}
