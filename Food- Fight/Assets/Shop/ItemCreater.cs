7df47d6d25652a5c9434dc4ee7c93b887fc2951f
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-10-26 22:45:28 +1300

    Update 1.10
    Added stuff for dan

---
+++ Food- Fight/Assets/Shop/ItemCreater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCreater : MonoBehaviour
+{
+
+    public shopItems itemType;
+
+    public Image sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
