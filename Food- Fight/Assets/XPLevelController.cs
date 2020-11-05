6d5c5d7e1703f9de93f6b6eabbd2019c419d775b
SerBABA <dan.r0n3n@gmail.com>
2020-11-05 23:02:20 +1300

    Update 1.0.1

---
+++ Food- Fight/Assets/XPLevelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPLevelController : MonoBehaviour
+{
+
+    public int currentXpLevel = 0;
+    public int maxXpLevel = 20;
+
+    
+    public int GetCurrentXpLevel()
+    {
+        return currentXpLevel;
+    }
+
+
+    public void ResetXpLevel()
+    {
+        currentXpLevel = 0;
+    }
+
+
+    public void DecreaseXpLevel(int amount)
+    {
+        currentXpLevel = Mathf.Max(0, );
+    }
+
+
+    public void IncreaseXpLevel(int amount)
+    {
+
+    }
+
+}
