f6e31787dcde55bc21466e666a67d3677cfbb012
vaan3225 <63621952+Raggii@users.noreply.github.com>
2021-07-20 22:30:47 +1200

    Update 1.0.1
    Fixed wall generation.
    Getting back into this we will finsih it

---
+++ food-fight/Assets/WallGenerator/objectSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectSpawn : MonoBehaviour
+{
+
+    public GameObject[] objects;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        if (objects.Length <= 0)
+        {
+            return;
+        }
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+
+    }
+
+}
